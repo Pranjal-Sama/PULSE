5587198a12d7062b023766f7a539849ea9b681eb
Pranjal Sharma <pranjal2k19@gmail.com>
2026-04-23 18:46:20 +0530

    Configure matplotlib to use non-interactive backend
    Added lines to set non-interactive backend for matplotlib.

--- pulse_heart_model.py
+++ pulse_heart_model.py
@@ -9,6 +9,12 @@
 # ============================================================
 import pandas as pd
 import numpy as np
+
+# --- ADD THESE TWO LINES BEFORE IMPORTING PYPLOT ---
+import matplotlib
+matplotlib.use('Agg') # Forces non-interactive backend to prevent thread crashes
+# ---------------------------------------------------
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
